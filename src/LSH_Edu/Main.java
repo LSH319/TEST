6de460bca51c1c0ea07d1e25842b636741aae112
LSH <LSH@DESKTOP-EA81K10>
2021-06-03 11:44:38 +0900

    First Practice

--- src/LSH_Edu/Main.java
+++ src/LSH_Edu/Main.java
@@ -1,7 +1,15 @@
 package LSH_Edu;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-	  System.out.println("hello");
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("¼·¾¾¿Âµµ : ");
+	double temp = scan.nextDouble(); 
+	Double Fahr = (temp * 1.8) + 32; //¿Âµµ º¯È¯°ø½Ä : (¼·¾¾ * 1.8) + 32
+	
+	System.out.println("È­¾¾¿Âµµ : " + Fahr);
   }
 }
