b15ef6e879581324b53e316888612f2f801ae936
LSH <LSH@DESKTOP-EA81K10>
2021-06-07 11:48:24 +0900

    Add InputMismatchException

--- src/LSH_Edu/Cal_Score.java
+++ src/LSH_Edu/Cal_Score.java
@@ -1,5 +1,6 @@
 package LSH_Edu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Cal_Score {
@@ -7,14 +8,21 @@ public class Cal_Score {
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("점수 입력 : ");
-		int score = scan.nextInt();
-		
-		System.out.print("학점 : ");
-		if(score >= 90) System.out.print("A");
-		else if(score >= 80) System.out.print("B");
-		else if(score >= 70) System.out.print("C");
-		else if(score >= 60) System.out.print("D");
-		else System.out.print("F");
-		scan.close();
+		try {
+			int score = scan.nextInt();
+			
+			System.out.print("학점 : ");
+			if(score >= 90) System.out.print("A");
+			else if(score >= 80) System.out.print("B");
+			else if(score >= 70) System.out.print("C");
+			else if(score >= 60) System.out.print("D");
+			else System.out.print("F");
+		}catch(InputMismatchException e) {
+			System.out.println("wrong value");
+			return;
+		}
+		finally {
+			scan.close();
+		}
 	}
 }
