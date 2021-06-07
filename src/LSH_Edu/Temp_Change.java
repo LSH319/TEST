9d314f66b54de451b7205102cc68f6eb7beb9548
LSH <LSH@DESKTOP-EA81K10>
2021-06-07 11:37:12 +0900

    Add InputMismatchException

--- src/LSH_Edu/Temp_Change.java
+++ src/LSH_Edu/Temp_Change.java
@@ -1,5 +1,6 @@
 package LSH_Edu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Temp_Change {
@@ -7,11 +8,16 @@ public class Temp_Change {
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("¼·¾¾¿Âµµ : ");
-		double Celsius = scan.nextDouble(); 
-		double Fahrenheit  = (Celsius * 1.8) + 32; //¿Âµµ º¯È¯°ø½Ä : (¼·¾¾ * 1.8) + 32
 		
-		System.out.println("È­¾¾¿Âµµ : " + Fahrenheit);
+		try {
+			double Celsius = scan.nextDouble(); 
+			double Fahrenheit  = (Celsius * 1.8) + 32; //¿Âµµ º¯È¯°ø½Ä : (¼·¾¾ * 1.8) + 32
+			System.out.println("È­¾¾¿Âµµ : " + Fahrenheit);
+		}catch(InputMismatchException e) {
+			System.out.println("wrong value");
+			return;
+		}
+		
 		scan.close();
 	}
-	
 }
