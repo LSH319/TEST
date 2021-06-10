bbfdbee49dba16c3ee6996aa01ec9470bbb8a4b2
LSH <LSH@DESKTOP-EA81K10>
2021-06-10 12:07:00 +0900

    BankTask Practice

---
+++ src/LSH_BankAccount_Practice/BankManager.java
@@ -0,0 +1,77 @@
+package LSH_BankAccount_Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class BankAccount{
+	String Name;
+	String Account;
+	int balance = 0;
+	
+	BankAccount(String Name,String Account){
+		this.Name = Name;
+		this.Account = Account;
+	}
+	
+	public void deposit(int amount) {
+		this.balance = this.balance + amount;
+	}
+	public void withdraw(int amount) {
+		if(this.balance < amount) {
+			System.out.println("출금액은 잔액을 넘길 수 없습니다.");
+		}
+		else {
+			this.balance = this.balance - amount;
+		}
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		this.Name = name;
+	}
+	public String getAccount() {
+		return Account;
+	}
+	public void setAccount(String account) {
+		this.Account = account;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+}
+
+public class BankManager {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BankTask bankTask = new BankTask();
+		
+		ArrayList<BankAccount> bankAccount = new ArrayList<BankAccount>();
+		while(true) {
+			System.out.println("0. 종료 \n1. 계좌개설 \n2. 입금 \n3. 출금\n4. 모든계좌출력");
+			int num = scan.nextInt();
+			
+			if(num == 1) {
+				bankTask.makeAccount(bankAccount);
+			}
+			else if(num == 2) {
+				bankTask.depositAndwithdrawal(bankAccount,num);
+			}
+			else if (num==3) {
+				bankTask.depositAndwithdrawal(bankAccount,num);
+			}
+			else if(num==4) {
+				bankTask.printAllAccount(bankAccount);
+			}
+			else if(num == 0) break;	
+		}
+		scan.close();
+	}
+}
---
+++ src/LSH_BankAccount_Practice/BankTask.java
@@ -0,0 +1,42 @@
+package LSH_BankAccount_Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BankTask {
+	Scanner scan = new Scanner(System.in);
+	
+	public void makeAccount(ArrayList<BankAccount> bankAccount) {
+		
+		
+		System.out.println("이름 : ");
+		String Name = scan.nextLine();
+		System.out.println("계좌번호 : ");
+		String Account = scan.nextLine();
+		
+		bankAccount.add(new BankAccount(Name,Account));
+	}
+	
+	public void depositAndwithdrawal(ArrayList<BankAccount> bankAccount,int menu) {
+		System.out.print("계좌번호 입력 : ");
+		String account = scan.nextLine();
+		if(account.equals("")) account = scan.nextLine();
+		for(BankAccount bankaccount : bankAccount) {
+			if(bankaccount.Account.equals(account)) {
+				System.out.print("금액 : ");
+				int amount = scan.nextInt();
+				if(menu == 2) bankaccount.deposit(amount);
+				else if(menu == 3) bankaccount.withdraw(amount);
+				return;
+			}
+		}
+		System.out.print("없는계좌\n");
+	}
+	
+	public void printAllAccount(ArrayList<BankAccount> bankAccount) {
+		for(BankAccount bankaccount : bankAccount) {
+			System.out.println(bankaccount.getName() + " " + bankaccount.getAccount() + " " + bankaccount.getBalance());
+		}
+	}
+}
